51ba6c31252efc703ec4d57db5f25d06163bfe19
Javayria <javayriamudassar@gmail.com>
2024-01-18 15:03:49 -0500

    changes:

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,15 +5,25 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
+    // create Options object
+    Options options = new Options();
+    // add an -i flag
+    options.addOption("i", false, "provided maze");
+
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
         try {
-            logger.info("**** Reading the maze from file " + args[0]);
+            // Parse the command line arguments//
+            CommandLineParser parser = new DefaultParser();
+            CommandLine commandline = parser.parse(options, args);
+ 
+            logger.info("**** Reading the maze from file " + args[i]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
             while ((line = reader.readLine()) != null) {
