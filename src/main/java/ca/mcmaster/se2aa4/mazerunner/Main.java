41953a116a0798ecd1dde2cf9eb611618df61f2a
Javayria <javayriamudassar@gmail.com>
2024-01-21 00:15:40 -0500

    Walking skeleton - created abstractions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,10 +1,11 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
+import java.io.IOException;
+import java.nio.file.Path;
+
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,37 +16,77 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            Configuration config = config(args);
+            logger.info(config);
+            Maze theMaze = new Maze(config.inputFile()); //generates the maze object
+            String path = null;
+            boolean correctPath = false;
 
-            Options options = new Options();
-            options.addOption("i", true, "provided mazse");
-            // Parse the command line arguments//
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
+            if(config.inputPath==null){
+                MazeSolver theSolver = new MazeSolver(theMaze); //chooses a method to solve the maze
+                path = theSolver.solve();
+                System.out.println(path);
+            } 
+            else{
+                PathCheck theCheck = new PathCheck(theMaze, config.inputPath());
+                correctPath = theCheck.result();
 
-            //extract based on flag
-            String inputFile = "default";
-            if(cmd.hasOption("i")) {
-                inputFile = cmd.getOptionValue("i");
+                if(correctPath){
+                    System.out.println("correct path");
+                }
+                else{
+                    System.out.println("incorrect path");
+                }
             }
-          
-            logger.info("**** Reading the maze from file " + inputFile);
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+            
+        } catch(ParseException pe) {
+            System.err.println(pe.getMessage());
+            System.exit(1);
+        }
+    }
+
+    //configuration method for command line arguments
+    private static Configuration config(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption("i", true, "must specify filepath (.txt) for maze");
+        options.addOption("p", true, "activate path sequence check for maze");
+        CommandLineParser parser = new DefaultParser();  // Parse the command line arguments
+        
+        CommandLine cmd = parser.parse(options, args);
+        String inputFile = null;
+        String inputPath = null;
+        if(cmd.hasOption("i")) { //validate that the -i flag is present in command
+            inputFile = cmd.getOptionValue("i");
+            if(cmd.hasOption("p")) { //check if -p flag is present 
+                inputPath = cmd.getOptionValue("p");
+            }
+        }
+        return new Configuration(inputFile, inputPath);        
+    }
+
+    //data class
+    private record Configuration(String inputFile, String inputPath) {
+        public Configuration {
+            File tmp = new File(inputFile);
+            if(!tmp.exists()) {
+                throw new IllegalArgumentException("Given file does not exist.");
+            } 
+            
+            //verify the path sequence as viable
+            if (inputPath != null){
+                boolean valid=true;
+                for (int i = 0 ; i < inputPath.length() ; i++) {
+                    char c = inputPath.charAt(i);
+                    if (!Character.isDigit(c)){
+                        if ((c != 'F') && (c != 'R') && (c != 'L')){
+                            valid = false;
+                        }
                     }
                 }
-                System.out.print(System.lineSeparator());
-            }
-        } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\");
+                if(valid == false){
+                    throw new IllegalArgumentException("Path sequence is not provided in a valid form.");
+                }
+            }  
         }
-        logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
-        logger.info("** End of MazeRunner");
-    }
+    }    
 }
