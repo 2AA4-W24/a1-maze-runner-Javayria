dee6e2a0ea4d81ddd885784f53b4e7e2bc588c7d
Javayria <javayriamudassar@gmail.com>
2024-01-25 03:00:02 -0500

    added new methods for Cell class #36

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Cell.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Cell.java
@@ -1,14 +1,24 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+//represents what type of cell it is within the maze grid
 public enum Cell {
     WALL('#'), PASS(' '), START('S'), END('E');
 
-    private final char cellType;
+    private final char character;
 
-    Cell(char cellType) {
-        this.cellType = cellType;
+    Cell(char character) {
+        this.character = character;
     }
 
+    public char getCharacter(){
+        return character;
+    }
+
+    public boolean equals(Cell cell){
+        return(character == cell.getCharacter());
+   }
+
     @Override
-    public String toString() { return "" + cellType; }    
+    public String toString() { return "" + character; }
+     
 }
