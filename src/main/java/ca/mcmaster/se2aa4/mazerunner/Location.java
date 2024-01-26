9623bc51b3213ef903029936e43f767f23eb3649
Javayria <javayriamudassar@gmail.com>
2024-01-25 23:29:35 -0500

    created Location class, which is extendable #26

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Location.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Location{
+    int x;
+    int y;
+
+    public Location(){
+        this.x = x;
+        this.y = y;  
+    }
+
+   //Position (direction + points) -> Location (direction) -> Direction (enum)
+
+
+    
+}
