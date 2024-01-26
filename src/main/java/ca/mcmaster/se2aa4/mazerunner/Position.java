175bba51c4c27cb8fcdd43f0a5b1e381e33e16fd
Javayria <javayriamudassar@gmail.com>
2024-01-25 23:24:51 -0500

    added Position class, which extends Location by adding Direction object #26

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
@@ -1,19 +1,31 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-public class Position {
-    int x;
-    int y;
+public class Position{
+    private Location coordinates;
+    private Direction direction;
     
-    public Position(int x, int y){
-        this.x = x;
-        this.y = y;
+    public Position(Location location){
+        this.coordinates = location; 
+        this.direction = Direction.NORTH;//PLACEHOLDERRRRRR
+    }
+
+    public boolean equals(Position position){
+        return ((this.coordinates == position.coordinates) && (this.direction == position.direction));
+    }
+
+    public Direction getDirection(){
+        return direction;
+    }
+
+    public Location getLocation(){
+        return coordinates;
     }
     
     @Override
     public String toString() {
-        return "Position: " + 
-            "( " + x +
-            " , " + y + 
-            " )";
+        return "Position: (" +
+         "location=" + coordinates + 
+            ", direction= " + direction + 
+            ")";
     }
 }
